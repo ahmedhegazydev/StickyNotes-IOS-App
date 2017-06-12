791afa6e4ecb6d29e19c1c11a5b3d6df08567c51
IOS Dev <iosdev@IOSs-Mac.local>
2017-06-12 00:55:50 -0700

    this is the first commit

---
+++ ToDoList IOS App/CustomTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableCell.swift
+//  ToDoList IOS App
+//
+//  Created by IOS Dev on 6/12/17.
+//  Copyright © 2017 IOS DevTest. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableCell: UITableViewCell {
+
+    @IBOutlet var textView: UITextView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+    
+    
+
+}
--- ToDoList IOS App/ViewController.swift
+++ ToDoList IOS App/ViewController.swift
@@ -8,8 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
 
+
+    @IBOutlet var tableView: UITableView!
+    
+    var items = [String]()
+    
+    @IBAction func addNewNote(_ sender: UIBarButtonItem) {
+        
+        
+        createAlert()
+        
+    }
+    
+    func createAlert(){
+        let alert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+        let add = UIAlertAction(title: "Add", style: .default){
+            (action) in
+            
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .default){
+            (alert) in
+            
+            
+        }
+        alert.addAction(add)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +56,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableCell
+        cell.textView.text = self.items[indexPath.section]
+        
+        return cell
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.items.count;
+    }
+    
+    
 }
 
